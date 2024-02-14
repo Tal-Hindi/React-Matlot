4339c38f48693f223a0b7149adcf86ed821720a1
Tal-Hindi <113764524+Tal-Hindi@users.noreply.github.com>
2024-02-14 15:53:54 +0200

    ss

--- HomeWork3/src/App.jsx
+++ HomeWork3/src/App.jsx
@@ -2,11 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Register from './Avia_FC/Register'
-import Login from './Avia_FC/Login'
+import Register from './Tal_FC/Register'
+import Login from './Tal_FC/Login'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
